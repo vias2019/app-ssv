a26d705cb85dc32757caea3130e5805a9fa9ac13
Victoria Grishkina <vias2010@gmail.com>
2019-09-11 21:50:03 -0700

    Modal and Chart Code

--- public/js/client2.js
+++ public/js/client2.js
@@ -1,30 +1,29 @@
 //See the trend button
 
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            //mysql
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    //mysql
+    ["Year", "Sales", "Expenses"],
+    ["2004", 1000, 400],
+    ["2005", 1170, 460],
+    ["2006", 660, 1120],
+    ["2007", 1030, 540]
+  ]);
 
-        var options = {
-          title: 'Airfare Trend for last 12 months',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
+  var options = {
+    title: "Airfare Trend for last 12 months",
+    curveType: "function",
+    legend: { position: "bottom" }
+  };
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+  var chart = new google.visualization.LineChart(
+    document.getElementById("curve_chart")
+  );
 
-        chart.draw(data, options);
-      }
+  chart.draw(data, options);
+}
 
-      module.exports=drawChart;
-    
-
-
+module.exports = drawChart;
--- public/js/sequelize.js
+++ public/js/sequelize.js
@@ -1,20 +1,23 @@
-
 // Sequelize (capital) references the standard library
 var Sequelize = require("sequelize");
 // sequelize (lowercase) references my connection to the DB.
 var sequelize = require("../../config/connection.js");
 
 // Creates a "Book" model that matches up with DB
-var Table = sequelize.define("chart", {
-  id: Sequelize.INTEGER,
-  months: Sequelize.STRING,
-  airfare: Sequelize.INTEGER,
-  pages: Sequelize.INTEGER,
-  destinationCity: Sequelize.STRING,
-  originCity: Sequelize.STRING
-},{
+var Table = sequelize.define(
+  "chart",
+  {
+    id: Sequelize.INTEGER,
+    months: Sequelize.STRING,
+    airfare: Sequelize.INTEGER,
+    pages: Sequelize.INTEGER,
+    destinationCity: Sequelize.STRING,
+    originCity: Sequelize.STRING
+  },
+  {
     freezeTableName: true
-});
+  }
+);
 
 // Syncs with DB
 Table.sync();
