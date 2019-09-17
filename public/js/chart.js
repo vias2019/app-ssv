f45855c0ffb4b3460532b4440f4e283ce7543e6b
Victoria Grishkina <vias2010@gmail.com>
2019-09-16 19:56:04 -0700

    fixed merging

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,21 +1,17 @@
 //See the trend button
 //var mysql =require("../../env");
 
-function chart()
-{
-  google.charts.load("current", { packages: ["corechart"] });
-  google.charts.setOnLoadCallback(drawChart);
 
 $("#button1").click(function (){
 
   google.charts.load("current", { packages: ["corechart"] });
   google.charts.setOnLoadCallback(drawChart);
-
-  // Send the GET request.
-  $.get("/api/chart", function (search) {
-    console.log("Got search:", search);
   
-    var data = google.visualization.arrayToDataTable(search);
+   // Send the GET request.
+   $.get("/api/chart", function (search) {
+    console.log("Got search:", search);
+    
+  var data = google.visualization.arrayToDataTable(search);
     // var data = google.visualization.arrayToDataTable([
     //   //mysql
     //   ["Year", "Sales"],
@@ -24,17 +20,17 @@ $("#button1").click(function (){
     //   ["2006", 660],
     //   ["2007", 1030]
     // ]);
-
+  
     var options = {
       title: "Airfare Trend for last 12 months",
       curveType: "function",
       legend: { position: "bottom" }
     };
-
+  
     var chart = new google.visualization.LineChart(
       document.getElementById("curve_chart")
     );
-
+  
     chart.draw(data, options);
   });
-});
+  });
