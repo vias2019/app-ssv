e332ff3c2db1b8c1f14a2e8dcb2fe8da1962e98b
Victoria Grishkina <vias2010@gmail.com>
2019-09-17 21:58:13 -0700

    chart

--- public/js/client.js
+++ public/js/client.js
@@ -119,24 +119,24 @@ function makeChart(data)
 }
 
 function drawChart()
-  {
-    var chartData = JSON.parse(localStorage.getItem("chartData"));
-    chartData = chartData.historical;
-    console.log("FOR GOOGLE");
-    console.log(chartData.historical);
-    var data = google.visualization.arrayToDataTable(chartData);
+{
+  var chartData = JSON.parse(localStorage.getItem("chartData"));
+  chartData = chartData.historical;
+  console.log("FOR GOOGLE");
+  console.log(chartData.historical);
+  var data = google.visualization.arrayToDataTable(chartData);
 
-    var options =
+  var options =
     {
       title: "Airfare Trend for the next 2 months",
       curveType: "function",
       legend: { position: "bottom" }
     };
 
-    var chart = new google.visualization.LineChart(document.getElementById("curve_chart"));
+  var chart = new google.visualization.LineChart(document.getElementById("curve_chart"));
 
-    chart.draw(data, options);
-  }
+  chart.draw(data, options);
+}
   
 
 
